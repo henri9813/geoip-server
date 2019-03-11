f48c28d4592b7ad1380752983d1551697bb320d8
henri9813 <henri.devigne@bonkgaming.fr>
2019-03-11 15:33:55 +0100

    Install GeoIP2

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+	"net"
+)
 
 func main()  {
 	fmt.Println("Starting server")
+
+	cityDatabase, err := loadDatabase("City")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	countryDatabase, err := loadDatabase("Country")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	asnDatabase, err := loadDatabase("ASN")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println(cityDatabase.City(net.ParseIP("8.8.8.8")))
+	fmt.Println(countryDatabase.Country(net.ParseIP("8.8.8.8")))
+	fmt.Println(asnDatabase.ASN(net.ParseIP("8.8.8.8")))
+}
+
+func loadDatabase(name string) (database *geoip2.Reader, err error){
+	db, err := geoip2.Open("data/GeoLite2-" + name + ".mmdb")
+	if err != nil {
+		return nil, errors.New("can't work with it")
+	}
+
+	return db, nil
 }
